5e6253f2faf6aeef6f25e3bb6b24b8fb8c4e634b
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-07-27 19:44:21 -0300

    Terminado productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,13 +26,73 @@ exports.getAllProducts = async (req, res) => {
 }
 
 exports.getProductById = async (req, res) => {
-    try{
+    try {
         const product = await Product.getProductById(req.params.product_id);
         if (!product || product.length < 0) {
-            return res.status(404).json({ error: 'No se ha encontrado ningun producto con ese ID'})
+            return res.status(404).json({ error: 'No se ha encontrado ningun producto con ese ID' })
         }
         return res.status(200).json(product);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al buscar el producto' })
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    const { error, value } = await updateProductSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const updated = await Product.updateProduct(req.params.product_id, value);
+        if (!updated) {
+            return res.status(404).json({ error: 'Producto no encontrado' })
+        }
+        return res.status(200).json({ message: 'Producto actualizado' })
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al actualizar el producto' })
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try{
+        const deleted = await Product.deleteProduct(req.params.product_id);
+        if(!deleted){
+            return res.status(404).json({ error: 'Producto no encontrado para eliminar'})
+        }
+        return res.status(200).json({ message: 'Producto desactivado' })
     }catch(error){
-        return res.status(500).json({ error: 'Error al buscar el producto'})
+        return res.status(500).json({ error: 'Error al eliminar el producto'})
+    }
+}
+
+exports.updateStock = async (req, res) => {
+    const {stock} = req.body;
+    if(typeof stock !== 'number' || stock < 0){
+        return res.status(400).json({ error: 'Stock invalido'});
+    }
+    try{
+        const updatedStock = await Product.updateStock(req.params.product_id, stock);
+        if(!updatedStock){
+            return res.status(404).json({ error: 'No se encontro el producto para actualizar el stock'})
+        }
+        return res.status(200).json(updatedStock);
+    }catch(error){
+        return res.status(500).json({ error: 'Error al actualizar el stock'})
+    }
+}
+
+exports.filterByCategory = async (req, res) => {
+    const filters = req.body;
+    
+    const {error, value} = filterProductsSchema.validate(filters);
+    if(error) return res.status(400).json({ error: error.details[0].message});
+
+    try{
+        const products = await Product.filterByCategory(value);
+        if(!products || products.length === 0) {
+            return res.status(404).json({ error: 'No se encontraron productos con esos filtros'});
+        }
+        return res.status(200).json(products);
+    }catch(error){
+        return res.status(500).json({ error: 'Error al filtrar productos'});
     }
 }
