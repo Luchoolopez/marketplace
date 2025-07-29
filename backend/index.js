32864932ac3422c7faaa82d8270020de19a95b53
LuchooLopez <Luchoolopez2810@gmail.com>
2025-07-29 18:34:26 -0300

    creado mas middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const {helmetMiddleware, morganMiddleware, limiter, notFoundHandler} = require('./middlewares/common');
+const errorHandler = require('./middlewares/errorHandler');
 const PORT = 3000;
 
 app.use(express.json()); //para parsear a JSON las peticiones
 app.use(cors()); 
+app.use(helmetMiddleware);
+app.use(morganMiddleware);
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.send('Hola mundo');
 });
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
---
+++ backend/middlewares/common.js
@@ -0,0 +1,22 @@
+const morgan = require('morgan');
+const helmet = require('helmter');
+const rateLimit = require('express-rate-limit');
+
+const helmetMiddleware = helmet();
+const morganMiddleware = morgan();
+const limiter = rateLimit({
+    windowMs: 15*60*1000, //15 minutos
+    max: 100 //maximo de 100 peticiones por IP
+});
+
+//middleware para rutas no encontradas
+function notFoundHandler(req, res, next){
+    res.status(404).json({ error: 'Error ruta no encontrada'});
+}
+
+module.exports = {
+    helmetMiddleware,
+    morganMiddleware,
+    limiter,
+    notFoundHandler
+};
