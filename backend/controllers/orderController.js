668cb2a92e21f6082dfdf47256ff99c18d7fe101
LuchooLopez <Luchoolopez2810@gmail.com>
2025-07-29 00:21:42 -0300

    Terminado ordenController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,14 +40,44 @@ exports.getOrderByUser = async (req, res) => {
     }
 }
 
-exports.getAllOrders = async(req, res) => {
-
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.getAllOrders();
+        if (!orders || orders.length === 0) {
+            return res.status(404).json({ error: ' No se ha encontrado ninguna orden' });
+        }
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al mostrar los productos' })
+    }
 }
 
-exports.updateOrderStatus = async(req, res) => {
-    
+exports.updateOrderStatus = async (req, res) => {
+    const { value, error } = await updateOrderStatusSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const updated = await Order.updateOrderStatus(req.params.order_id, value);
+        if (!updated) {
+            return res.status(404).json({ error: ' Orden no encontrada o no actualizada ' })
+        }
+        return res.status(200).json({ message: ' Status de la orden actualizado' })
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al actualizar la orden' })
+    }
 }
 
-exports.deleteOrder = async(req, res) => {
-    
+exports.deleteOrder = async (req, res) => {
+    const { error } = await deleteOrderSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const deleted = await Order.deleteOrder(req.params.order_id);
+        if (!deleted) {
+            return res.status(404).json({ error: 'Orden no encontrada para eliminar' });
+        }
+        return res.status(200).json({ message: 'Orden eliminada con exitoso' });
+    } catch (error) {
+        return res.status(500).json({ error: 'Error al eliminar la orden' })
+    }
 }
