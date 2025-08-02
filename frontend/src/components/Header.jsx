1683d1f4930aa5af42486dcc2ef5f15e33c987f1
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-08-02 15:32:42 -0300

    haciendo estilos

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
 import { IoSearchCircle } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import '../styles/Header.css'
 
 const Header = () => {
+    const [showSearch, setShowSearch] = useState(false);
+    const searchRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event){
+            if(searchRef.current && !searchRef.current.contains(event.target)){
+                setShowSearch(false);
+            }
+        }
+        if(showSearch){
+            document.addEventListener('mousedown', handleClickOutside);
+        }else{
+            document,removeEventListener('mousedown', handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [showSearch]);
+
+
+
     return(
         <div className="header-container">
             <div className="header-container-img">
-                <img></img>
+                <img src="/images/kira_store_logo_white.png" alt="kira_logo_store"></img>
             </div>
             <div className="header-container-links">
                 <a>Inicio</a>
@@ -17,10 +37,20 @@ const Header = () => {
                 <a>Contacto</a>
             </div>
             <div className="header-container-btn">
-                <a><IoSearchCircle/></a>
+                <a onClick={() => setShowSearch(!showSearch)}><IoSearchCircle/></a>
                 <a><FaUserCircle/></a>
                 <a><FaCartShopping/></a>
             </div>
+            {showSearch && (
+                <input
+                    ref={searchRef}
+                    type="text"
+                    className={`header-seach-input${showSearch ? " visible" : ""}`}
+                    placeholder="Buscar"
+                    autoFocus={showSearch}
+                    style={{display: showSearch ? 'flex' : 'flex'}}
+                />
+            )}
         </div>
     );
 }
