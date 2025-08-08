9ed2bb0ab68383cf91b9f86ebec2fccdf108aa6b
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-08-08 20:19:28 -0300

    Agregando img en home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Product from './Products'
 import Layout from '../components/Layout'
 import { useAuth } from '../hooks/useAuth'
+import '../styles/Home.css';
+
 
 const Home = () => {
   const { user } = useAuth();
 
   return (
-    <div>
+    <div className='home_page_container'>
       <Layout/>
+      <div className="img-container">
+        <img src='/images/home_page_wallpaper.jpg' alt='home_wallpaper_page' className='home_image'></img>
+      </div>
+
       <div className="home-container">
       { user?.role === 'admin' ? (
         <div className='home-title'>
